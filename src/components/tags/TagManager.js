79065cd013918ddb6c4ae82e9229372eb23be568
Myriam Chevalier <chevalier.myriam@hotmail.com>
2021-10-28 14:37:34 -0500

    added createNewTag() function in TagManager.js

--- src/components/tags/TagManager.js
+++ src/components/tags/TagManager.js
@@ -3,3 +3,14 @@ export const getAllTags = () => {
     return fetch("http://localhost:8088/tags")
         .then(res => res.json())
 }
+
+export const createNewTag = (newTag) => {
+    return fetch("http://localhost:8088/tags", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(newTag)
+    })
+        .then(getAllTags)
+}
